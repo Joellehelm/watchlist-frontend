78f095597620bb4a3be6be500c02dd27fc6970d3
joellehelm <joellenhelm@gmail.com>
2020-09-02 16:45:10 -0500

    style fixes

--- src/actions/watchlistActions.js
+++ src/actions/watchlistActions.js
@@ -2,8 +2,8 @@ import * as action from './actionTypes'
 
 
 export const getWatchlist = () => dispatch => {
-    fetch(`http://localhost:3000/watchlist`, {
-        // fetch(`https://xmarkbackend.herokuapp.com/watchlist`, {
+    // fetch(`http://localhost:3000/watchlist`, {
+        fetch(`https://xmarkbackend.herokuapp.com/watchlist`, {
         
         method: "GET",
         headers: {
@@ -23,8 +23,8 @@ export const getWatchlist = () => dispatch => {
 
 export const getShowProgress = (user_id, imdbID) => dispatch => {
 
-    fetch(`http://localhost:3000/get_progress`, {
-        // fetch(`https://xmarkbackend.herokuapp.com/get_progress`, {
+    // fetch(`http://localhost:3000/get_progress`, {
+        fetch(`https://xmarkbackend.herokuapp.com/get_progress`, {
         method: "POST",
         headers: {
             "Access-Control-Allow-Origin": "*",
@@ -36,8 +36,8 @@ export const getShowProgress = (user_id, imdbID) => dispatch => {
     })
         .then(r => r.json())
         .then(progress => {
-            fetch(`http://localhost:3000/shows/${imdbID}`, {
-                // fetch(`https://xmarkbackend.herokuapp.com/shows/${imdbID}`, {   
+            // fetch(`http://localhost:3000/shows/${imdbID}`, {
+                fetch(`https://xmarkbackend.herokuapp.com/shows/${imdbID}`, {   
                 method: "GET",
                 headers: {
                     "Access-Control-Allow-Origin": "*",
--- src/components/EpisodeTable.js
+++ src/components/EpisodeTable.js
@@ -13,8 +13,8 @@ class EpisodeTable extends Component {
         const progressExists = this.props.progress.showProgress.progress.find(p => p.episode_id === ep.id)
 
         if (progressExists) {
-            fetch(`http://localhost:3000/progresses/${progressExists.id}`, {
-                // fetch(`https://xmarkbackend.herokuapp.com/progresses/${progressExists.id}`, {
+            // fetch(`http://localhost:3000/progresses/${progressExists.id}`, {
+                fetch(`https://xmarkbackend.herokuapp.com/progresses/${progressExists.id}`, {
                 method: "DELETE",
                 headers: {
                     "Access-Control-Allow-Origin": "*",
@@ -28,8 +28,8 @@ class EpisodeTable extends Component {
                 this.props.getShowProgress(this.props.auth.user.id, this.props.progress.showProgress.show.show.imdbID)
             })
         } else {
-            fetch("http://localhost:3000/progresses", {
-            // fetch("https://xmarkbackend.herokuapp.com/progresses", {
+            // fetch("http://localhost:3000/progresses", {
+            fetch("https://xmarkbackend.herokuapp.com/progresses", {
                 method: "POST",
                 headers: {
                     "Access-Control-Allow-Origin": "*",
